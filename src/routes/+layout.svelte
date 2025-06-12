7fb8172b081a411a283aef8659b9d18d5808dff1
chathura-de-silva <ac.chathura@gmail.com>
2025-06-12 17:30:57 +0530

    feat: root page ui with responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,39 +26,43 @@
 
 <NavigationMenu.Root
 	viewport={false}
-	class="bg-background/80 flex max-w-full items-center justify-between border-b
-		px-6 py-4 shadow-sm  backdrop-blur"
+	class="bg-background/80 fixed top-0 right-0 left-0 z-50
+		flex max-w-full items-center justify-between border-b px-6 py-4 shadow-sm backdrop-blur"
 >
-	<NavigationMenu.List>
-		<NavigationMenu.Item class={cn(page.url.pathname === baseRoutes.app && 'bg-accent rounded-lg')}>
+	<NavigationMenu.List class="relative">
+		<NavigationMenu.Item
+			class={cn('font-semibold', page.url.pathname === baseRoutes.app && 'bg-accent rounded-lg')}
+		>
 			<NavigationMenu.Link href={baseRoutes.app}>App</NavigationMenu.Link>
 		</NavigationMenu.Item>
 
-		<NavigationMenu.Item>
+		<NavigationMenu.Item class="relative">
 			<NavigationMenu.Trigger
 				class={cn(
 					page.url.pathname.startsWith(baseRoutes.supportedDevices) && 'bg-accent rounded-lg'
 				)}>Supported Devices</NavigationMenu.Trigger
 			>
-			<NavigationMenu.Content>
-				<ul class="grid w-[200px] gap-4 p-2">
-					<li>
-						{#each supportedBrands as brand (brand.path)}
-							<NavigationMenu.Link
-								href={baseRoutes.supportedDevices + brand.path}
-								class={cn(
-									page.url.pathname.startsWith(baseRoutes.supportedDevices + brand.path) &&
-										'bg-accent rounded-lg'
-								)}>{brand.name}</NavigationMenu.Link
-							>
-						{/each}
-					</li>
-				</ul>
+			<NavigationMenu.Content
+				class="bg-background/80 absolute top-full left-0 mt-2 min-w-[200px] rounded-lg border p-2 shadow-lg backdrop-blur-sm md:w-auto"
+			>
+				{#each supportedBrands as brand (brand.path)}
+					<NavigationMenu.Link
+						href={baseRoutes.supportedDevices + brand.path}
+						class={cn(
+							'block w-full',
+							page.url.pathname.startsWith(baseRoutes.supportedDevices + brand.path) &&
+								'bg-accent rounded-lg'
+						)}>{brand.name}</NavigationMenu.Link
+					>
+				{/each}
 			</NavigationMenu.Content>
 		</NavigationMenu.Item>
 
 		<NavigationMenu.Item
-			class={cn(page.url.pathname.startsWith(baseRoutes.about) && 'bg-accent rounded-lg')}
+			class={cn(
+				'font-semibold',
+				page.url.pathname.startsWith(baseRoutes.about) && 'bg-accent rounded-lg'
+			)}
 		>
 			<NavigationMenu.Link href={baseRoutes.about}>About</NavigationMenu.Link>
 		</NavigationMenu.Item>
@@ -75,4 +79,6 @@
 	</NavigationMenu.List>
 </NavigationMenu.Root>
 
-{@render children()}
+<div class="mt-[73px]">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
-<!-- <script lang="ts">
-
+<script lang="ts">
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 </script>
 
+<div class="flex h-[calc(100vh-theme(spacing.24))] flex-col p-8 lg:flex-row">
+	<div class="border-ring relative h-full w-full rounded-lg border-3 border-dashed md:w-3/5">
+		<Skeleton class="h-full w-full opacity-0" />
+		<div class="absolute inset-0 z-10 flex items-center justify-center">
+			Click Here or Drag and Drop your Image
+		</div>
+	</div>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
- -->
+	<div class="flex h-full w-full flex-col p-4 md:w-2/5">
+		<div class="flex flex-1 basis-1/5 items-center justify-center text-center">
+			<h1 class="text-xl font-bold">Shutter Count 👇</h1>
+		</div>
+
+		<div class="flex flex-1 basis-4/5 items-center justify-center text-center">
+			<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">12,546</h1>
+		</div>
+	</div>
+</div>
